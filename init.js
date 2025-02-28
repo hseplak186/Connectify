d736b71d25a6f66f6708b8f591f345bcb92d961a
Kalpesh Paste <pastekalpesh@gmail.com>
2025-02-28 18:20:25 +0530

    Removed node modules

---
+++ init.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const Chat = require("./models/chat.js");
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/Connectify');
+}
+main()
+.then(() => {
+  console.log("Connection Successful") ; 
+})
+.catch((err) => console.log(err));
+
+
+let allChats = [
+    {
+        from:"Radha",
+        to:"Ram",
+        message:"Lets go for picnic",
+        created_at: new Date()
+    },
+    {
+        from: "Rahul",
+        to: "Simran",
+        message: "Did you finish the project report?",
+        created_at: new Date()
+    },
+    {
+        from:"Muskan",
+        to:"Kalpesh",
+        message:"Hey, how are you?",
+        created_at: new Date()
+    },
+    {
+        from: "Anjali",
+        to: "Vikram",
+        message: "Don't forget the meeting at 3 PM.",
+        created_at: new Date()
+    },
+    {
+        from: "Meera",
+        to: "Arjun",
+        message: "Good luck with your presentation!",
+        created_at: new Date()
+    },
+    {
+        from: "Maheshwar",
+        to:"Raj",
+        message:"Lets play cricket today",
+        created_at: new Date()
+    }
+]
+
+Chat.insertMany(allChats);
